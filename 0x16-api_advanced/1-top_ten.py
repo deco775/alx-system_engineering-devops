85b30cedec33d44da2105b0cfc336f399008ebae
deco775 <mkansipitso@gmail.com>
2024-08-13 17:06:12 +0000

    Advance API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-
+'''
+    this module contains the function top_ten
+'''
 import requests
 from sys import argv
 
 
 def top_ten(subreddit):
-    
+    '''
+        returns the top ten posts for a given subreddit
+    '''
     user = {'User-Agent': 'Lizzie'}
     url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
                        .format(subreddit), headers=user).json()
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
+"""Contains recurse function"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    
+    """Returns a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "0x16-api_advanced:project:\
